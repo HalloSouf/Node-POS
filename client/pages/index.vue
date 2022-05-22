a1a05b130bdfb27ba8ad12c194e3678f5e1a0814
Soufiane <soufianesedd@gmail.com>
2022-05-22 22:06:28 +0200

    Client setup

---
+++ client/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
